8886699202a58a68135ecdff674671f2cbfff18b
Nicolly Rocha <nicollyrcs@gmail.com>
2024-05-28 00:04:40 -0300

    commiting

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -11,11 +11,4 @@ router.get('/time-control/:id', TimeControlController.getTimeControlByUser);
 
 router.put('/time-control/:id', TimeControlController.updateTimeControlByUser);
 
-router.get('/', async (req: any, res: any) => {
-	await db.query(
-		'CREATE TABLE user_pontos (id SERIAL PRIMARY KEY, userid VARCHAR(100), starttime TIMESTAMP, endtime TIMESTAMP)'
-	);
-	res.send('Success!');
-});
-
 export { router };
